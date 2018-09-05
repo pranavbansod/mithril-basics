ad964f4a1582071ae78ab66819bf3b7bc8ee12d7
Pranav Bansod <pranavb@thoughtworks.com>
2018-09-05 14:38:16 +0530

    Removed statements from todo view.

--- src/views/EditItem.js
+++ src/views/EditItem.js
@@ -1,15 +1,20 @@
-var m = require("mithril")
-var toDo = require("../models/ToDo")
+var m = require("mithril");
+var toDo = require("../models/ToDo");
 
 module.exports = {
-  oninit: function(vnode) {toDo.load(vnode.attrs.id)},
-    view:function () {
-        return m("form", [
-            m("label.label", "Description"),
-            m("input.input[type=text][placeholder=Write description for ToDo item]",{value:toDo.currentItem.desc}),
-            m("button.button[type=button]",{onclick: function () {
-
-              }}, "Save")
-        ]);
-    }
+  oninit: function (vnode) {
+    toDo.load(vnode.attrs.id)
+  },
+  view: function () {
+    return m("form", [
+      m("label.label", "Description"),
+      m("input.input[type=text][placeholder=Write description for ToDo item]", {id:"toDoDesc",value: toDo.currentItem.desc}),
+      m("button.button[type=button]", {
+        onclick: function () {
+          toDo.currentItem.desc = document.getElementById("toDoDesc").value;
+          console.log(toDo);
+        }
+      }, "Save")
+    ]);
+  }
 };
--- src/views/ToDo.js
+++ src/views/ToDo.js
@@ -2,12 +2,9 @@ const m = require("mithril");
 let ToDo = require("../models/ToDo");
 
 module.exports = {
-    view: function () {
-        let getDescForItem = function (item) {
-            return m("a.toDo-item",{href: "/edit/" + item.id, oncreate: m.route.link},item.desc);
-        };
-        let title = m("h1",ToDo.title);
-        let itemDivs = ToDo.items.map(getDescForItem);
-        return m(".toDo",title,itemDivs);
-    }
+  view: function (vnode) {
+    return m(".toDo", m("h1", ToDo.title), ToDo.items.map(function (item){
+      return m("a.toDo-item", {href: "/edit/" + item.id, oncreate: m.route.link}, item.desc);
+    }));
+  }
 };
